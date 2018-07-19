0daaf34b31123d0409ff9ac6f890a2371a353442
Kitcat22 <41338562+Kitcat22@users.noreply.github.com>
2018-07-19 15:44:23 +0100

    Jago

---
+++ Programming Assignment 2.R
@@ -0,0 +1,32 @@
+makeCacheMatrix <- function(x = matrix()) {
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(inverse) inv <<- inverse
+        getInverse <- function() inv
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
+}
+
+cacheSolve <- function(x, ...) {
+        inv <- x$getInverse()
+        if (!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data)
+        x$setInverse(inv)
+        inv
+}
+
+B <-makeCacheMatrix(matrix(1:4, 2, 2))
+B$get()
+B$getInverse()
+cacheSolve(B)
+B$getInverse()
+B$set(matrix(4, 2, 1, 3))
